57825749630c8bc348d15fc3d528ec546054c469
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-05-19 16:57:24 -0300

    criacao da tabela imc

--- imc-calculator/src/App.js
+++ imc-calculator/src/App.js
@@ -1,3 +1,4 @@
+import Table from './Components/Table/Table';
 
 import './App.css';
 
@@ -5,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Tabela IMC</h1>
+        <Table/>
       </header>
     </div>
   );
